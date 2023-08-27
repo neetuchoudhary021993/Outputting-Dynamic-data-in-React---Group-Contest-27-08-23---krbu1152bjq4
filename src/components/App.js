b718b6e2debf768c17b38cf78c121f8dc514e241
yamunar04 <135696530+yamunar04@users.noreply.github.com>
2023-08-27 19:47:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -39,12 +39,13 @@ const App = () => {
   function final() {
     setName(dataN);
   }
+return(
   <div id="main">
       <input id="input" onChange={(e) => store(e)}></input>
       <button id="button" onClick={final}> Click </button>
       <p id="text"> Hello my name is {name} and I study at Newton School</p>
     </div>
-  
+  );
 };
 
 export default App;
