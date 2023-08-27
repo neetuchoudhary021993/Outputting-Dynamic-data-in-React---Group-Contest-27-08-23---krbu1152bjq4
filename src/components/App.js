013cc80a314217040198cc63923fd1d2ffff193a
yamunar04 <135696530+yamunar04@users.noreply.github.com>
2023-08-27 19:32:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
-import '../styles/App.css';
+// import React, { useState } from 'react';
+// import '../styles/App.css';
 
-const App = () => {
-  const [name, setName] = useState('');
-  const [displayText, setDisplayText] = useState(''); // State to store the display text
+// const App = () => {
+//   const [name, setName] = useState('');
+//   const [displayText, setDisplayText] = useState(''); // State to store the display text
 
-  const handleButtonClick = () => {
-    setDisplayText(` Hello my name is ${name} and I study at Newton School`);
-  };
+//   const handleButtonClick = () => {
+//     setDisplayText(` Hello my name is ${name} and I study at Newton School`);
+//   };
 
-  return (
-    <div id="main">
-      <input
-        id="input"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <button id="button" onClick={handleButtonClick}>Click</button>
+//   return (
+//     <div id="main">
+//       <input
+//         id="input"
+//         onChange={(e) => {
+//           setName(e.target.value);
+//         }}
+//       />
+//       <button id="button" onClick={handleButtonClick}>Click</button>
       
-      <p id="text">{displayText}</p>
+//       <p id="text">{displayText}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css';
+const App = () => {
+//code here
+  const [name, setName] = useState("____");
+  let dataN = "";
+  function store(e) {
+    dataN = e.target.value;
+  }
+  function final() {
+    setName(dataN);
+  }
+  <div id="main">
+      <input id="input" onChange={(e) => store(e)}></input>
+      <button id="button" onClick={final}> Click </button>
+      <p id="text"> Hello my name is {name} and I study at Newton School</p>
     </div>
   );
-}
+};
 
 export default App;
-
