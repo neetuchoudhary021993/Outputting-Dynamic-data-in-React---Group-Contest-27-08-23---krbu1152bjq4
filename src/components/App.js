3416debb437943033ac1c587cd8653fa0c1d29a0
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-27 18:36:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 const[name, setName] = useState("");
-const onChange = () => {
-  setName(e.target.value);
-}
+
+    const handleButtonClick = () => {
+    const textPara = document.getElementById('text');
+    textPara.textContent = `Hello my name is ${name} and I study at Newton School`;
+  };
+
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
+      <input id='input' onChange={(e) => {setName(e.target.value)}></input>
+      <button id='button' onClick={handleButtonClick}>Click</button>
       <p id='text'> Hello my name is {name} and I study at Newton School</p>
     </div>
   )
